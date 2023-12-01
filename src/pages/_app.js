9ebbef4acd16d22043498447fbdc56678c72626e
Zach Sanchez <zachsanchez00@gmail.com>
2023-12-01 12:43:49 -0800

    vercel analytics, weeeeee

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import Layout from '@/components/Layout/Layout'
 import '@/styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics/>
     </Layout>
   )
 }
